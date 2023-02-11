29725f4a2a0417554f828ec759daa8aa2ad93d33
Wali Muhammad <luckywali0986@gmail.com>
2023-02-11 18:03:28 +0500

    import all the blogs from asset for using

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+
+import blog01 from '../../assets/blog01.png';
+import blog02 from '../../assets/blog02.png';
+import blog03 from '../../assets/blog03.png';
+import blog04 from '../../assets/blog04.png';
+import blog05 from '../../assets/blog05.png';
+
 export default function Blog() {
   return (
     <div>
