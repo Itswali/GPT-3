42a8e8c9cd1a8c26bae48ed0f85e760e4f5ff90f
Wali Muhammad <luckywali0986@gmail.com>
2023-03-04 23:51:13 +0500

    fix mail and image

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -39,7 +39,7 @@ export default function Footer() {
           <h4>Get in touch</h4>
           <p>Islamabasd, Pakistan</p>
           <p>+923095925592</p>
-          <p>luckywali0987@gmail.com</p>
+          <p>luckywali0986@gmail.com</p>
         </div>
       </div>
 
